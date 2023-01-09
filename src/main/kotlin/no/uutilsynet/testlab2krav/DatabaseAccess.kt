d2d030e9d273d0f0c50e856b973e6d7c3b44bb6a
Lars Lønne <lonnelars@gmail.com>
2023-01-09 15:01:55 +0100

    returnerer app-navn for GET /
    DFK-74

--- src/main/kotlin/no/uutilsynet/testlab2krav/DatabaseAccess.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/DatabaseAccess.kt
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class DatabaseAccess(val kravDao: KravDao) {
 
-  @GetMapping("/") fun getDatabaseData(): List<Krav> = kravDao.listKrav()
+  @GetMapping("/krav") fun getDatabaseData(): List<Krav> = kravDao.listKrav()
 }
