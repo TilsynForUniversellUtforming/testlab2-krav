124b14f70afec03112714c0738ef573ca1de8b0e
Lars Lønne <lonnelars@gmail.com>
2023-01-09 15:01:55 +0100

    returnerer app-navn for GET /
    DFK-74

--- src/main/kotlin/no/uutilsynet/testlab2krav/Testlab2KravApplication.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/Testlab2KravApplication.kt
@@ -2,9 +2,16 @@ package no.uutilsynet.testlab2krav
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication class Testlab2TestingApplication
 
 fun main(args: Array<String>) {
   runApplication<Testlab2TestingApplication>(*args)
+}
+
+@RestController
+class AppNameResource {
+  @GetMapping("/") fun appName() = mapOf("appName" to "testlab2-krav")
 }
