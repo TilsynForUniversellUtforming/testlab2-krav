4c2142fda37bb0ab02e37c51e12662a9a472d7da
oho <oystein.hoyum@digdir.no>
2023-12-22 13:22:19 +0100

    DFK-431

--- src/main/kotlin/no/uutilsynet/testlab2krav/krav/KravApi.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/krav/KravApi.kt
@@ -1,7 +1,20 @@
 package no.uutilsynet.testlab2krav.krav
 
 import no.uutilsynet.testlab2krav.dto.Krav
+import no.uutilsynet.testlab2krav.dto.KravWcag2x
 
 interface KravApi {
     fun listKrav(): List<Krav>
+
+    fun listWcagKrav(): List<KravWcag2x>
+
+    fun getWcagKrav(id: Int): KravWcag2x
+
+    fun getKravBySuksesskriterium(suksesskriterium: String): KravWcag2x
+
+    fun createWcagKrav(krav: KravWcag2x): Int
+
+    fun deleteKrav(kravId: Int): Boolean
+
+    fun updateWcagKrav(krav: KravWcag2x): Int
 }
--- src/main/kotlin/no/uutilsynet/testlab2krav/krav/KravResource.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/krav/KravResource.kt
@@ -2,13 +2,29 @@ package no.uutilsynet.testlab2krav.krav
 
 import no.uutilsynet.testlab2krav.dao.KravDAO
 import no.uutilsynet.testlab2krav.dto.Krav
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import no.uutilsynet.testlab2krav.dto.KravWcag2x
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("v1/krav")
 class KravResource(val kravDao: KravDAO) : KravApi {
 
     @GetMapping override fun listKrav(): List<Krav> = kravDao.listKrav()
+
+    @GetMapping("wcag2krav") override fun listWcagKrav(): List<KravWcag2x> = kravDao.listWcagKrav()
+
+    @GetMapping("wcag2krav/{id}")
+    override fun getWcagKrav(id: Int): KravWcag2x = kravDao.getWcagKrav(id)
+
+    @GetMapping("suksesskriterium/{suksesskriterium}")
+    override fun getKravBySuksesskriterium(suksesskriterium: String): KravWcag2x =
+        kravDao.getKravBySuksesskriterium(suksesskriterium)
+
+    @PostMapping("wcag2krav")
+    override fun createWcagKrav(@RequestBody krav: KravWcag2x): Int = kravDao.createWcagKrav(krav)
+
+    @DeleteMapping override fun deleteKrav(kravId: Int): Boolean = kravDao.deleteKrav(kravId)
+
+    @PutMapping("wcag2krav/{id}")
+    override fun updateWcagKrav(krav: KravWcag2x): Int = kravDao.updateWcagKrav(krav)
 }
