160ea84ae53ba6cc181e1c5b6968f220a8dc1030
oho <oystein.hoyum@digdir.no>
2024-01-19 14:10:56 +0100

    DFK-431 slått i saman krav tabellar krav-tabellar

--- src/main/resources/db/migration/V3__merge_krav_tables.sql
+++ src/main/resources/db/migration/V3__merge_krav_tables.sql
@@ -1,10 +1,11 @@
 alter table Wcag2Krav
 add column tittel varchar(255) not null default '',
 add column status varchar(255) not null default '',
-add column innhald varchar(255) not null default '',
+add column innhald text not null default '',
 add column gjeldautomat boolean not null default false,
+add column gjeldnettsider boolean not null default false,
 add column gjeldapp boolean not null default false,
-add column urlrettleiing varchar(255) not null default '';
+add column urlrettleiing varchar(255)  default '';
 
 update Wcag2Krav set tittel = Krav.tittel, status = Krav.status, innhald = Krav.innhald, gjeldautomat = Krav.gjeldautomat, gjeldapp = Krav.gjeldapp, urlrettleiing = Krav.urlrettleiing
 from Krav
