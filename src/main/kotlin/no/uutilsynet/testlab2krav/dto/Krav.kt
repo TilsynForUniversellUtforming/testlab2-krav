94cb1cc85a36eda8e8217cfa6eec210c603b3bc7
oho <oystein.hoyum@digdir.no>
2024-09-24 15:22:01 +0200

    DFK-431 Betre feilhandtering av tomt resultat ved oppslag

--- src/main/kotlin/no/uutilsynet/testlab2krav/dto/Krav.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/dto/Krav.kt
@@ -1,12 +1,20 @@
 package no.uutilsynet.testlab2krav.dto
 
+import java.net.URL
+
 open class Krav(
     open val id: Int,
     open val tittel: String,
-    open val status: String,
+    open val status: KravStatus,
     open val innhald: String?,
     open val gjeldAutomat: Boolean,
     open val gjeldNettsider: Boolean,
     open val gjeldApp: Boolean,
-    open val urlRettleiing: String?,
+    open val urlRettleiing: URL?,
 )
+
+enum class KravStatus(val status: String) {
+    nytt("Nytt"),
+    gjeldande("Gjeldande"),
+    utgaatt("Utgått")
+}
--- src/main/kotlin/no/uutilsynet/testlab2krav/dto/KravWcag2x.kt
+++ src/main/kotlin/no/uutilsynet/testlab2krav/dto/KravWcag2x.kt
@@ -1,16 +1,21 @@
 package no.uutilsynet.testlab2krav.dto
 
+import java.net.URL
+import no.uutilsynet.testlab2.constants.WcagPrinsipp
+import no.uutilsynet.testlab2.constants.WcagRetninglinje
+import no.uutilsynet.testlab2.constants.WcagSamsvarsnivaa
+
 data class KravWcag2x(
     override val id: Int,
     override val tittel: String,
-    override val status: String,
+    override val status: KravStatus,
     override val innhald: String?,
     override val gjeldAutomat: Boolean,
     override val gjeldNettsider: Boolean,
     override val gjeldApp: Boolean,
-    override val urlRettleiing: String?,
-    val prinsipp: String,
-    val retningslinje: String,
+    override val urlRettleiing: URL?,
+    val prinsipp: WcagPrinsipp,
+    val retningslinje: WcagRetninglinje,
     val suksesskriterium: String,
     val samsvarsnivaa: WcagSamsvarsnivaa
 ) :
@@ -24,17 +29,3 @@ data class KravWcag2x(
         gjeldApp,
         urlRettleiing,
     )
-
-enum class WcagSamsvarsnivaa(val nivaa: String) {
-    A("A"),
-    AA("AA"),
-    AAA("AAA")
-}
-
-enum class WcagRetninglinje(val retninglinje: String) {
-    tidsbasert_media("1.2 Tidsbasert media")
-}
-
-enum class WcagPrinsipp(val prinsipp: String) {
-    mulig_aa_oppfatte("1. Mulig å oppfatte")
-}
